5ac013d3cc7a3ae1e546206f03a0d21407e4d7a4
abeer19955 <abeerali19955@gmail.com>
2023-09-02 11:22:13 +0400

    ss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'weather'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('weather');
+    
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather';
+export class AppComponent implements OnInit {
+  city: string = '';
+  weatherData: any;
+  error: string = '';
+  
+  constructor(private weatherService: WeatherService) {}
+
+  
+
+  ngOnInit(): void {}
+
+  getWeather(){
+    
+    
+      this.weatherService.getWeather(this.city).subscribe(data=>{
+        this.weatherData=data;
+      console.log(data);
+      this.error='';
+      },
+    
+    (error) => {
+      
+      if (error) {
+        this.error = 'City not found. Please check the city name.';
+      } else {
+        this.error = 'An error occurred while fetching weather data.';
+      }
+    }
+  );
 }
+kelvinToCelsius(kelvin: number): number {
+  return kelvin - 273.15;
+}
+}
+ 
